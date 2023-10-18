ce953926319dc3b294f4e906b4a075adc16cda7f
System 16 <mittuparmar19@gmail.com>
2023-10-18 10:20:47 +0530

    mediaQuery Added

--- lib/all_screens.dart
+++ lib/all_screens.dart
@@ -27,7 +27,6 @@ class AllScreens extends StatefulWidget {
 }
 
 class _AllScreensState extends State<AllScreens> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,146 +34,128 @@ class _AllScreensState extends State<AllScreens> {
         title: const Text('All Screens'),
       ),
       body: ListView(
-           children: [
-             ListTile(
-               title: const Text('Dismissible Demo'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const DismissibleDemo()));
-               },
-             ),
-             ListTile(
-               title: const Text('Example-3'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const Example3()));
-               },
-             ),
-             ListTile(
-               title: const Text('Get Data From Api with Get-x'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const LoginPage()));
-               },
-             ),
-             ListTile(
-               title: const Text('Home'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const Home()));
-               },
-             ),
-             ListTile(
-               title: const Text('ImagePicker with Get-x'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const ImagePickerDemo()));
-               },
-             ),
-             ListTile(
-               title: const Text('Index'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const GetXEx()));
-               },
-             ),
-             ListTile(
-               title: const Text('MarkFavList'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const MarkFavouriteApp()));
-               },
-             ),
-             ListTile(
-               title: const Text('Screen-1'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const S1()));
-               },
-             ),
-             ListTile(
-               title: const Text('Screen-2'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const S2()));
-               },
-             ),
-             ListTile(
-               title: const Text('Screen-3'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const S3()));
-               },
-             ),
-             ListTile(
-               title: const Text('Graphs & Charts Demo'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const Chart()));
-               },
-             ),
-             ListTile(
-               title: const Text('  Demo'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const DiffWidgetsInFlutter()));
-               },
-             ),
-             ListTile(
-               title: const Text('Drag & Drop List'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const DragAndDropListDemo()));
-               },
-             ),
-             ListTile(
-               title: const Text('Drag & Drop Widget'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const DragAndDropGrid()));
-               },
-             ),
-             ListTile(
-               title: const Text('Drag & Drop Widget'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-
-                 Navigator.of(context).push(MaterialPageRoute(
-                     builder: (context) => const DemoApp()));
-               },
-             ),
-
-
-           ],
-
+        children: [
+          ListTile(
+            title: const Text('Dismissible Demo'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const DismissibleDemo()));
+            },
+          ),
+          ListTile(
+            title: const Text('Example-3'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const Example3()));
+            },
+          ),
+          ListTile(
+            title: const Text('Get Data From Api with Get-x'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const LoginPage()));
+            },
+          ),
+          ListTile(
+            title: const Text('Home'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => const Home()));
+            },
+          ),
+          ListTile(
+            title: const Text('ImagePicker with Get-x'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const ImagePickerDemo()));
+            },
+          ),
+          ListTile(
+            title: const Text('Index'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const GetXEx()));
+            },
+          ),
+          ListTile(
+            title: const Text('MarkFavList'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const MarkFavouriteApp()));
+            },
+          ),
+          ListTile(
+            title: const Text('Screen-1'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => const S1()));
+            },
+          ),
+          ListTile(
+            title: const Text('Screen-2'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => const S2()));
+            },
+          ),
+          ListTile(
+            title: const Text('Screen-3'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => const S3()));
+            },
+          ),
+          ListTile(
+            title: const Text('Graphs & Charts Demo'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => const Chart()));
+            },
+          ),
+          ListTile(
+            title: const Text('  Demo'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const DiffWidgetsInFlutter()));
+            },
+          ),
+          ListTile(
+            title: const Text('Drag & Drop List'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const DragAndDropListDemo()));
+            },
+          ),
+          ListTile(
+            title: const Text('Drag & Drop Widget'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const DragAndDropGrid()));
+            },
+          ),
+          ListTile(
+            title: const Text('Animated Text'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const DemoApp()));
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/demo.dart
+++ lib/demo.dart
@@ -7,23 +7,37 @@ class DemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-body: Row(
-  children: [
-    AnimatedTextKit(
-      animatedTexts: [
-        FadeAnimatedText(
-          'Fade First',
-          textStyle: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-        ScaleAnimatedText(
-          'Then Scale',
-          textStyle: const TextStyle(fontSize: 20.0, fontFamily: 'Canterbury'),
-        ),
-        ColorizeAnimatedText('Color ', textStyle: const TextStyle(fontSize: 20), colors: [ Colors.redAccent,Colors.greenAccent,Colors.blueAccent])
-      ],
-    ),
-  ],
-),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: AnimatedTextKit(
+              animatedTexts: [
+                FadeAnimatedText(
+                  'Fade First',
+                  textStyle: const TextStyle(
+                      fontSize: 50.0, fontWeight: FontWeight.bold),
+                ),
+                ScaleAnimatedText(
+                  'Then Scale',
+                  textStyle:
+                      const TextStyle(fontSize: 50.0,),
+                ),
+                ColorizeAnimatedText('Then Color ',
+                    textStyle: const TextStyle(fontSize: 50),
+                    colors: [Colors.purple, Colors.yellow, Colors.green],speed:const Duration(milliseconds: 1000)),
+                TypewriterAnimatedText('Type This',
+                    textStyle: const TextStyle(fontSize: 50),speed:const  Duration(milliseconds: 1000)),
+                WavyAnimatedText('Wavy Text',
+                    textStyle: const TextStyle(fontSize: 50),speed: const Duration(milliseconds:1000 )),
+                RotateAnimatedText('Rotate Text',
+                    textStyle: const TextStyle(fontSize: 50),duration: const Duration(milliseconds: 1000))
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
